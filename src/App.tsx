5f1562d8841ef75f8bb89fc60420cad85352f2be
JungEun <foreverkiddd@gmail.com>
2025-04-07 17:30:29 +0900

    250407 jsx 문법

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+{/* 주석문 작성합니다. */}
+{/*
+  작성자 : lje
+  작성일 : 2025.04.07.
+  내용 : 기능에 대한 내용
+ */}
+
 function App() {
+  // const style = {
+  //   backgroundColor : 'black',
+  //   color : 'white',
+  //   fontSize : '48px',
+  //   fontWeight : 'bold',
+  //   padding : '20px'
+  // }
+
+  let name = "리액트";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="test">Hello, 
+        {
+          name === '리액트' ? (<h1>YES</h1>):null
+        }!!</h1>
+        {/* <p>반갑습니다.</p> */}
+        
     </div>
   );
+
+  // const port = undefined;
+
+  // return(
+  //   <div>
+  //     { 
+  //       port || '3000'
+  //     }
+  //   </div>
+  // )
 }
+
+// function App() {
+//   return React.createElement("div", null, "Hello, 리액트!!",
+//   React.createElement("p", null, "반갑습니다.") );
+// }
 
 export default App;
